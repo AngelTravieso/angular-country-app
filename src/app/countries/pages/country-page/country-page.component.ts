c732b6ce358f65acfcda6f3ef7e37090872eaf5f
AngelTravieso <angeltraviesoc@gmail.com>
2023-09-11 16:55:20 -0400

    switchMap - Refactorizacion

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -12,19 +13,35 @@ export class CountryPageComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private countriesService: CountriesService
+    private router: Router,
+    private countriesService: CountriesService,
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe( ({ id }) => {
+      .pipe(
+        // Tap sirve para disparar efectos secundarios
+        // tap()
+        // Regresa un nuevo observable
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ) )
+      )
+      .subscribe( ( country ) => {
+        // console.log({country});
+        if ( !country ) {
+          // Si no tenemos un pais navega a la URL indicada
+          return this.router.navigateByUrl('');
+        }
 
-        this.countriesService.searchCountryByAlphaCode( id )
-          .subscribe(country => {
-            console.log(country);
-          })
-
+        console.log('tenemos un pais');
+        return;
       });
+  }
+
+  searchCountry( code: string ) {
+    this.countriesService.searchCountryByAlphaCode( code )
+    .subscribe(country => {
+      console.log(country);
+    });
   }
 
 }
