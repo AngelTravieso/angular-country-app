ace91ac56359fd4caaa3b4790d6492b106d13996
AngelTravieso <angeltraviesoc@gmail.com>
2023-09-11 14:40:16 -0400

    Countries Service - Restcountries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+
     // RouterModule
     AppRoutingModule,
-
+    BrowserModule,
+    HttpClientModule,
     SharedModule,
   ],
   providers: [],
