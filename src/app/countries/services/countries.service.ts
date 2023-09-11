d533baf9e43c70dfaf8cb8d3d321db671b2e6259
AngelTravieso <angeltraviesoc@gmail.com>
2023-09-11 16:35:09 -0400

    Argumentos por URL - Pagina de pais

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -12,6 +12,14 @@ export class CountriesService {
 
     constructor(private http: HttpClient) { }
 
+    searchCountryByAlphaCode( code: string ): Observable<Country[]>  {
+        const url = `${ this.apiUrl }/alpha/${ code }`;
+        return this.http.get<Country[]>( url )
+            .pipe(
+                catchError( error => of([] ))
+            );
+    }
+
     // Buscar por capital
     searchCapital( term: string ): Observable<Country[]> {
         const url = `${ this.apiUrl }/capital/${ term }`;
